5b4586a4584673ca236f891d8d9eb8101c7a0b2e
alnutile <alfrednutile@gmail.com>
2019-02-25 21:06:09 -0500

    first component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,3 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -7,8 +6,8 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
 
+window.Vue = require('vue');
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -17,10 +16,19 @@ window.Vue = require('vue');
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
+
+
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('dashboard-wrapper', require('./components/DashboardWrapper.vue').default);
+
+
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
---
+++ resources/js/components/DashboardWrapper.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <div class="p-2">Wrapper</div>
+  </section>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log("Component mounted.");
+  }
+};
+</script>
